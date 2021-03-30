3c0ca37c59d2ede6b6d82e9b3009931b38454db0
nvectorrr <nav2373@mail.ru>
2021-03-30 12:51:42 +0300

    Initial Commit

---
+++ Dogsign ToDo/Dogsign_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dogsign_ToDoApp.swift
+//  Dogsign ToDo
+//
+//  Created by Виктор  Найденович  on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dogsign_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
